7a3f9c3134105e2487714bd5dc321697cfb6d22a
Cheng Adrian <chengadrian@ChengdeMacBook-Pro.local>
2022-11-05 17:54:36 +0000

    database change

--- server/routes/webpage.js
+++ server/routes/webpage.js
@@ -3,7 +3,8 @@ const express = require("express");
 const webpageRouter = express.Router();
 const mustache = require("mustache");
 const fs = require("fs").promises;
-const db = require("../../database-temp/db.js");
+const {db, getPaste, insertPaste, getRows} = require("../../database-temp/db.js");
+
 //Load any component
 (async () => {
   recentPastesView = await fs.readFile(
@@ -12,28 +13,36 @@ const db = require("../../database-temp/db.js");
   );
 })();
 //render homepage
-webpageRouter.get("/", async (req, res) => {
+webpageRouter.get("/", (req, res) => {
   let sql = "select content from pastes order by created desc limit 3";
-  let rows = await db.getRows(sql);
-  console.log(rows[0]);
-  let recentPastes = mustache.render(recentPastesView, {
-    1: rows[0].content,
-    2: rows[1].content,
-    3: rows[2].content,
+  getRows(db, sql, [], (rows)=>{
+    console.log(rows[0]);
+    let recentPastes = mustache.render(recentPastesView, {
+      1: rows[0].content,
+      2: rows[1].content,
+      3: rows[2].content,
+    });
+    res.render("../pages/home", { recentPastes: recentPastes });
   });
-  res.render("../pages/home", { recentPastes: recentPastes });
+  
 });
 
 //For static viewing of submitted pastes.
-webpageRouter.get("/view/:pasteID", async (req, res) => {
-  let pasteContent = await db.getPaste(req.params.pasteID);
-  console.log(pasteContent);
-  res.render("../pages/viewpaste", { pid: pasteContent.content });
+webpageRouter.get("/view/:pasteID", (req, res) => {
+  // let pasteContent = await db.getPaste(req.params.pasteID);
+  getPaste(db, req.params.pasteID, (pasteContent)=>{
+    console.log('pasteContent')
+    console.log(pasteContent);
+    res.render("../pages/viewpaste", { pid: pasteContent.content });
+  })
 });
 
 //Posts
-webpageRouter.post("/pastes/new", async (req, res) => {
-  let newRow = await db.insertPaste(req.body.content);
-  res.json({ content: req.body.content, id: newRow.lastID });
+webpageRouter.post("/pastes/new", (req, res) => {
+  // let newRow = await db.insertPaste(req.body.content);
+  insertPaste(db, req.body.content, 1, 0, (newRow)=>{
+    console.log(newRow);
+    res.json({ content: req.body.content, id: newRow.id });
+  })
 });
 module.exports = webpageRouter;
