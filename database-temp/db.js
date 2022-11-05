e00eb72165bc9271043c0819b482797e978f595d
cleverdumb <97397944+cleverdumb@users.noreply.github.com>
2022-11-05 18:05:33 +0000

    database change (#26)
    Co-authored-by: Cheng Adrian <chengadrian@ChengdeMacBook-Pro.local>

--- database-temp/db.js
+++ database-temp/db.js
@@ -1,44 +1,72 @@
 const sqlite3 = require("sqlite3").verbose();
-const { open } = require("sqlite");
+// const { open } = require("sqlite");
 
-async function openDB() {
-  // open the database
-  return await open({
-    filename: "./database-temp/develop.db",
-    driver: sqlite3.Database,
-  });
+// async function openDB() {
+//   // open the database
+//   return await open({
+//     filename: "./database-temp/develop.db",
+//     driver: sqlite3.Database,
+//   });
+// }
+
+// //returns the content of a paste from the ID
+// async function getPaste(id) {
+//   let db = await openDB();
+//   const result = await db.get("SELECT content from pastes where id=?", id);
+//   db.close();
+//   return result;
+// }
+
+// //returns the row of the created paste
+// async function insertPaste(content, userid = 1, private = 0) {
+//   let db = await openDB();
+//   const result = await db.run(
+//     "INSERT INTO pastes (user_id,content,private) VALUES (?,?,?)",
+//     userid,
+//     content,
+//     private
+//   );
+//   db.close();
+//   return result;
+// }
+// async function getRows(sql) {
+//   let db = await openDB();
+//   let rows = await db.all(sql);
+//   db.close();
+//   return rows;
+// }
+
+const db = new sqlite3.Database('./database-temp/develop.db', sqlite3.OPEN_READWRITE, (err)=>{
+  if (err) throw err;
+})
+
+function getPaste(db, id, cb) {
+  db.get('select content from pastes where id=?', [id], (err,row)=>{
+    if (err) throw err;
+    console.log('row')
+    console.log(row);
+    cb(row);
+  })
 }
 
-//returns the content of a paste from the ID
-async function getPaste(id) {
-  let db = await openDB();
-  const result = await db.get("SELECT content from pastes where id=?", id);
-  db.close();
-  return result;
+function insertPaste(db, content, userid = 1, private = 0, cb) {
+  db.get('insert into pastes (user_id, content, private) values (?,?,?) returning rowId', [userid, content, private], (err,row)=>{
+    if (err) throw err;
+    cb(row);
+  })
 }
 
-//returns the row of the created paste
-async function insertPaste(content, userid = 1, private = 0) {
-  let db = await openDB();
-  const result = await db.run(
-    "INSERT INTO pastes (user_id,content,private) VALUES (?,?,?)",
-    userid,
-    content,
-    private
-  );
-  db.close();
-  return result;
-}
-async function getRows(sql) {
-  let db = await openDB();
-  let rows = await db.all(sql);
-  db.close();
-  return rows;
+function getRows(db, sql, args=[], cb) {
+  db.all(sql, args, (err,rows)=>{
+    if (err) throw err;
+    cb(rows);
+  })
 }
 
-const db = {
-  getRows: getRows,
-  insertPaste: insertPaste,
-  getPaste: getPaste,
+const dbExp = {
+  db,
+  getRows,
+  insertPaste,
+  getPaste,
 };
-module.exports = db;
+module.exports = dbExp;
